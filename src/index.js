f8482dfd1e880cab96dd1a603d0696ace324a174
Øystein Kambo Tangerås <oystein.tangeras@retriever.no>
2018-05-11 09:11:44 +0200

    removed unused import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { Settings } from 'luxon';
 
 import './index.css';
