bbf652668155bcc2e80b63ec61229bd4d9d07992
Øystein Kambo Tangerås <tangeraas@gmail.com>
2019-07-21 20:25:50 +0200

    updatede eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { createHttpLink } from 'apollo-link-http';
 import { Settings } from 'luxon';
 
 import './index.css';
@@ -11,12 +10,8 @@ import { unregister} from './registerServiceWorker';
 
 Settings.defaultLocale = 'nn';
 
-
-const httpLink = createHttpLink({uri: "https://api.entur.io/journey-planner/v2/graphql"});
-
 const client = new ApolloClient({
 	uri: "https://api.entur.io/journey-planner/v2/graphql",
-	link: httpLink,
 });
 
 
